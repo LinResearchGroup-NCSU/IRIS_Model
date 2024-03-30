99f9a1d0f842b3ff6082cba8a6187b2e81835a51
Xingcheng Lin <xclin@xclins-imac.local>
2024-03-30 11:17:10 -0400

    first commit

---
+++ 1st_attempt/2bu1_P/testing/optimization/for_bindingE/2bu1/cmd.sh
@@ -0,0 +1,12 @@
+#########################################################################
+# Author: Xingcheng Lin
+# Created Time: Sun Jun 14 16:58:44 2020
+# File Name: cmd.sh
+# Description: 
+#########################################################################
+#!/bin/bash
+
+bash target_rna.sh 2bu1
+
+cd ../loocv
+bash cmd.copyFile.sh
