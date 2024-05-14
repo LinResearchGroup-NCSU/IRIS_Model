8fbafc686397252bef3699847ddd8d94d18ac6b7
eacisner <152431856+eacisner@users.noreply.github.com>
2024-05-14 00:57:20 -0400

    eduardo_folder

---
+++ eduardo/training_eduardo/PDBs/cmd.renumberresidue.sh
@@ -0,0 +1,4 @@
+/Users/eduardo/Downloads/mayachemtools/bin/ModifyPDBFiles.pl --overwrite --mode RenumberResidues --ResidueNumberMode Sequential 1aq3_modified.pdb 
+cp  1aq3_modifiedRenumberResidues.pdb 1aq3_modified.pdb 
+
+/Users/eduardo/Downloads/mayachemtools/bin/ModifyPDBFiles.pl --overwrite --mode RenumberResidues --ResidueNumberMode Sequential 1aq3_Rmodified.pdb cp 1aq3_RmodifiedRenumberResidues.pdb 1aq3_Rmodified.pdb
